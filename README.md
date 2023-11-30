9a7cb8c645963442a5756f1f40163f5692b942b4
patrikpordi <ppordi@umd.edu>
2023-11-30 06:22:36 -0500

    README has been initialized, partially filled

---
+++ README.md
@@ -0,0 +1,85 @@
+# Author: Patrik Dominik Pördi
+# ROS Humble C++ PubSub Example
+
+## Overview
+
+This ROS 2 package, has been created an autonomous movement by the turtlebot with obstacle avoidance
+
+## Prerequisites
+
+Before using this package, make sure you have the following dependencies:
+
+- ROS 2 Humble (Installation guide: [ROS 2 Installation](https://docs.ros.org/en/humble/Installation.html))
+- [Colcon](https://colcon.readthedocs.io/en/released/)
+- C++17 compatible compiler
+
+
+Additionally, for simulation purposes, you will need to install the following ROS packages:
+
+```bash
+# Install Gazebo ROS packages for TurtleBot simulation
+sudo apt -y install ros-humble-gazebo-ros-pkgs
+
+# Install TurtleBot 3 packages
+sudo apt -y install ros-humble-turtlebot3*
+```
+
+## Building and Installation
+
+To build the package, follow these steps:
+
+1. Clone the package into your ROS 2 workspace's `src/` directory:
+
+   ```bash
+  
+
+   ----
+To bag all the topics during launch use
+```bash
+ros2 ros2 launch ros2-turtlebot launch.py rosbag_record:=true
+```
+OR
+Without bagging
+```bash
+ros2 launch ros2-turtlebot launch.py
+```
+OR
+```bash
+# To update the frequency of the talker and change the save location of the bag file
+ros2 launch cpp_pubsub talker_listener_bagger.py talker_f:=10.0 bag_file:='./src/cpp_pubsub/results/new_recording'
+```
+
+This will run the talker node and bag all the topics it publishes for 15sec
+> :warning: A new bag will not be created if a file with the same name already exists
+
+To replay the bag file along with a listener node run the following command
+```bash
+# This will play the bag file recorded in the results path 
+ros2 launch cpp_pubsub talker_listener_bagger.py replay_only:='True' bag_file:='./src/cpp_pubsub/results/new_recording'
+```
+Run the unit tests by running the command 
+```bash
+colcon test --packages-select cpp_pubsub
+cat log/latest_test/cpp_pubsub/stdout_stderr.log
+```
+
+## CppLint & CppCheck
+   ```bash
+   # Use the below command for cpp lint by moving to root directory of your workspace 
+   cpplint  --filter=-build/c++11,+build/c++17,-build/namespaces,-build/include_order $( find . -name *.cpp | grep -vE -e "^(./build/|./install/|./log/)" ) &> results/cpplint.txt
+
+   # Use the below command for cpp check by moving to root directory of your workspace
+   cppcheck --enable=all --std=c++17 --suppress=missingIncludeSystem $( find . -name *.cpp | grep -vE -e "^(./build/|./install/|./log/)" ) --check-config  &> results/cppcheck.txt
+```
+
+
+
+## Results
+
+Can be found results/
+   cppcheck.txt
+   cpplint.txt
+   rqt_console.png
+   frames_2023-11-19_17.22.55.gv
+   frames_2023-11-19_17.22.55.pdf
+   new_recording
--- launch/launch.py
+++ launch/launch.py
@@ -1,3 +1,37 @@
+# from launch_ros.actions import Node
+# from launch import LaunchDescription
+# from ament_index_python.packages import get_package_share_directory
+# from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+# from launch.conditions import IfCondition
+# from launch.actions import ExecuteProcess
+# from launch.substitutions import TextSubstitution
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+# from launch.substitutions import LaunchConfiguration
+# import os
+
+# def generate_launch_description():
+#     return LaunchDescription([
+#     DeclareLaunchArgument('rosbag_record', default_value = 'false', choices = ['true', 'false'], description = "Enable rosbag recording"),
+#         IncludeLaunchDescription(
+#             PythonLaunchDescriptionSource([
+#                 os.path.join(get_package_share_directory(
+#                     'turtlebot3_gazebo'), 'launch'), '/turtlebot3_world.launch.py'
+#             ])
+#         ),
+#         # Launch the walker node
+#         Node(
+#             package='ros2-turtlebot',
+#             executable='walking',
+#             name='Walking_node',
+#             output='screen'
+#         ),
+#             ExecuteProcess(
+#             condition=IfCondition(LaunchConfiguration('rosbag_record')),
+#             cmd=['ros2', 'bag', 'record', '-o','walker_bag','-a','-x','depth_cam'],
+#             shell=True
+#         )
+#     ])
+
 from launch_ros.actions import Node
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
@@ -11,11 +45,17 @@ import os
 
 def generate_launch_description():
     return LaunchDescription([
-    DeclareLaunchArgument('rosbag_record', default_value = 'false', choices = ['true', 'false'], description = "Enable rosbag recording"),
+        # Set the TURTLEBOT3_MODEL environment variable
+        DeclareLaunchArgument('turtlebot3_model', default_value='waffle', description='TurtleBot3 model'),
+        ExecuteProcess(
+            cmd=['export', f'TURTLEBOT3_MODEL={LaunchConfiguration("turtlebot3_model")}'],
+            shell=True,
+            name='set_turtlebot3_model'
+        ),
+        DeclareLaunchArgument('rosbag_record', default_value='false', choices=['true', 'false'], description='Enable rosbag recording'),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
-                os.path.join(get_package_share_directory(
-                    'turtlebot3_gazebo'), 'launch'), '/turtlebot3_world.launch.py'
+                os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch', 'turtlebot3_world.launch.py')
             ])
         ),
         # Launch the walker node
@@ -25,9 +65,9 @@ def generate_launch_description():
             name='Walking_node',
             output='screen'
         ),
-            ExecuteProcess(
+        ExecuteProcess(
             condition=IfCondition(LaunchConfiguration('rosbag_record')),
-            cmd=['ros2', 'bag', 'record', '-o','walker_bag','-a','-x','depth_cam'],
+            cmd=['ros2', 'bag', 'record', '-o', 'walker_bag', '-a', '-x', 'depth_cam'],
             shell=True
         )
     ])
